cbe635a95816f14d4d3f84976f8e6b041d723e4a
Shantralee-58 <lindiwekhumalo833@gmail.com>
2023-12-01 12:29:13 -0800

    A Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Fetches https://alx-intranet.hbtn.io/status
+"""
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
+
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(utf8_content))
