a0f7108e0274fae27da82c1e6d9a7cd83cf7ffd1
Shantralee-58 <lindiwekhumalo833@gmail.com>
2023-12-03 03:36:47 -0800

    updated

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,8 +12,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
     with urllib.request.urlopen(url, data=data) as response:
         content = response.read().encode('utf-8')
-    print("Your email is: {}".format(content))
+        print("Your email is: {}".format(content))
