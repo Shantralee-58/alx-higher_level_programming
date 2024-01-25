a055a346dba919196ad898561721b64b0cf947fb
Shantralee-58 <lindiwekhumalo833@gmail.com>
2024-01-25 12:10:06 -0800

    Write a function that finds a peak in a list of unsorted integers.

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,9 +9,19 @@ def find_peak(list_of_integers):
     """
     if not list_of_integers:
         return None
-    list_of_integers.sort()
-    return list_of_integers[-1]
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
 
 """
-Time Complexity: O(nlog(n))
+Time Complexity: O(log(n))
 """
