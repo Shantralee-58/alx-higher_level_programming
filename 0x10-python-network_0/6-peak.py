4bf0c6067e72567afdb9354249edd1f5e06afdc0
Shantralee-58 <lindiwekhumalo833@gmail.com>
2024-01-25 13:19:11 -0800

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
-""" Find the peak on unsorted list """
+"""script for finding peak in list of ints, interview prep
+"""
+
+"""
+    THOUGHT PROCESS
+        it is not sorted, so sorting would take n(log(n))
+            -> not worth sorting
+        looping through and keeping track of max (brute force)
+            -> O(n)
+
+        possibly looping from each end reducing to 1/2 run time
+            -> still O(n)
+"""
 
 
 def find_peak(list_of_integers):
-    """ Function to find the peak on unsorted list of numbers """
-    if list_of_integers is None or not type(list_of_integers) is list:
-        return None
-    if len(list_of_integers) == 0:
-        return None
-
-    i = 0
-    peak = list_of_integers[0]
-    list_len = len(list_of_integers)
-
-    while (i < list_len - 1):
-        if i == 0 and list_of_integers[i] >= list_of_integers[i + 1]:
-            peak = max(peak, list_of_integers[i])
-            i += 1
-        elif (list_of_integers[i] >= list_of_integers[i - 1] and
-              list_of_integers[i] >= list_of_integers[i + 1]):
-            peak = max(peak, list_of_integers[i])
-            i += 1
-
-        i += 1
-    try:
-        if list_of_integers[i] >= list_of_integers[i - 1]:
-            peak = max(peak, list_of_integers[i])
-    except Exception:
-        pass
-    return peak
+    """BRUTE force implementation for question
+    """
+    max_i = None
+    for ele in list_of_integers:
+        if max_i is None or max_i < ele:
+            max_i = ele
+    return max_i
