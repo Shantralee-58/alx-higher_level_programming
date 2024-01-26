c71a244984431c8db87a60aaed6bcf36bf01cce7
Shantralee-58 <lindiwekhumalo833@gmail.com>
2024-01-26 13:49:55 -0800

    Write a Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Takes in a URL, sends a request to the URL, and displays the body of the response.
+Handles urllib.error.HTTPError exceptions and prints the HTTP status code in case of an error.
+"""
+
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
