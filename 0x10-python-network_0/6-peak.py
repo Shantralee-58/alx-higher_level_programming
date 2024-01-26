fff11f7b72175bdf9f460ed365160130084c6c87
Shantralee-58 <lindiwekhumalo833@gmail.com>
2024-01-26 01:34:56 -0800

    corrected

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
-"""Script for finding peak in list of ints, interview prep
+"""script for finding peak in list of ints, interview prep
 """
 
+"""
+    THOUGHT PROCESS
+        it is not sorted, so sorting would take n(log(n))
+            -> not worth sorting
+        looping through and keeping track of max (brute force)
+            -> O(n)
+
+        possibly looping from each end reducing to 1/2 run time
+            -> still O(n)
+"""
+
+
 def find_peak(list_of_integers):
-    """Binary search implementation for finding a peak
+    """BRUTE force implementation for question
     """
-
-    if not list_of_integers:
-        return None
-
-    n = len(list_of_integers)
-
-    if n == 1:
-        return list_of_integers[0]
-
-    start, end = 0, n - 1
-
-    while start < end:
-        mid = (start + end) // 2
-
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            end = mid
-        else:
-            start = mid + 1
-
-    return list_of_integers[start]
-
-# Example usage:
-# peak = find_peak([1, 2, 4, 6, 3])
-# print(peak)
+    max_i = None
+    for ele in list_of_integers:
+        if max_i is None or max_i < ele:
+            max_i = ele
+    return max_i
