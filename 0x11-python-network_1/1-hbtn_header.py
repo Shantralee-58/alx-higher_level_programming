d44bf009ea0fdeaa5fb83b30707c1fcbf80ec150
Shantralee-58 <lindiwekhumalo833@gmail.com>
2024-01-26 05:58:14 -0800

    updated

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,5 +8,13 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:
-        print(response.getheader("X-Request-Id"))
+    req = urllib.request.Request(url)
+    
+    try:
+        with urllib.request.urlopen(req) as response:
+            x_request_id = response.getheader("X-Request-Id")
+            print(x_request_id)
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
+    except Exception as e:
+        print("An error occurred:", e)
