c5b5f464bb80aab8aa7c06544abdab2f970cb185
Shantralee-58 <lindiwekhumalo833@gmail.com>
2024-01-26 05:30:32 -0800

    updated

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,27 +1,13 @@
 #!/usr/bin/python3
-"""Script for finding peak in list of ints using binary search.
-   Time Complexity: O(log(n))
+"""Script for finding peak in list of ints using sorting.
+   Time Complexity: O(n log n)
 """
 
 def find_peak(list_of_integers):
-    """Binary search implementation for finding a peak
+    """Sort the list in descending order and return the first element as the peak
     """
     if not list_of_integers:
         return None
 
-    n = len(list_of_integers)
-
-    if n == 1:
-        return list_of_integers[0]
-
-    start, end = 0, n - 1
-
-    while start < end:
-        mid = (start + end) // 2
-
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            end = mid
-        else:
-            start = mid + 1
-
-    return list_of_integers[start]
+    list_of_integers.sort(reverse=True)
+    return list_of_integers[0]
