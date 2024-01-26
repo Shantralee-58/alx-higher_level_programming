daf64e4e250d08a6d0b32ee04917bcb7d4c0ed08
Shantralee-58 <lindiwekhumalo833@gmail.com>
2024-01-26 14:24:08 -0800

    Write a Python script that takes in a URL, sends a request to the URL and displays the body of the response.

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Takes in a URL and an email address, sends a POST request
-to the passed URL with the email as a parameter,
+Takes in a URL and an email address, sends a POST
+request to the passed URL with the email as a parameter,
 and finally displays the body of the response.
 """
 
